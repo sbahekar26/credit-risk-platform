45e9a8ca9ec3aaf550e4bb45a71cbadf693cd73e
Siddharth Bahekar <siddharthbahekar13@gmail.com>
2026-07-21 12:16:25 -0400

    Refactor: extract CreditRisk.Core shared library, add solution file

--- Applicant.cs
+++
@@ -1,7 +0,0 @@
-public class Applicant
-{
-    public required string FullName { get; set; }
-    public int Age { get; set; }
-    public decimal AnnualIncome { get; set; }
-    public int CreditScore {get; set; }
-}
