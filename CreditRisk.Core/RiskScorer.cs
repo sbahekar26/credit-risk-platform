785c64d28d1c251cc8617b571ec9b1b647babb44
Siddharth Bahekar <siddharthbahekar13@gmail.com>
2026-07-21 13:29:10 -0400

    Add Blazor UI with loan application form

--- CreditRisk.Core/RiskScorer.cs
+++ CreditRisk.Core/RiskScorer.cs
@@ -4,6 +4,10 @@ public class RiskScorer
     {
         //hard rule: age 18+, credit score 650+, loan amount < 50% of annual income
         //soft rule: purpose should be in line with person's background, credit score of 550-650 acceptable but needs review
+        if (application.Applicant.AnnualIncome <= 0)
+    {
+        return RiskDecision.Decline;
+    }
         decimal ratio = application.LoanAmount / application.Applicant.AnnualIncome;
         if (application.Applicant.Age < 18)
         {
